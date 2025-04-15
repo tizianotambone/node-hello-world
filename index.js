60d693f30a12a364db63085c14aecc5b1601a2cc
tizianotambone <tambonetiziano@gmail.com>
2025-04-15 15:33:04 +0200

    [UPD]genero una nuova costante per memorizzare il terzo argomento

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ console.log("hello world")
 //  0       1          2
 // node index.js "Hello World!"
 const arg2 = process.argv[2]
-console.log(arg2)
+// genero una nuova costante per memorizzare il terzo argomento
+const arg3 = process.argv[3]
+console.log(arg2,arg3)
